7ec1dabac8041311c61dadd011cdc0ace7d23d8d
Hemkala <thapaaliza258@gmail.com>
2026-09-08 17:24:11 +0545

    Add win-line detection

--- script.js
+++ script.js
@@ -15,10 +15,21 @@ function buildBoard(){
   }
 }
 
+const WIN_LINES = [
+  [0,1,2],[3,4,5],[6,7,8],
+  [0,3,6],[1,4,7],[2,5,8],
+  [0,4,8],[2,4,6]
+];
+
+function checkWin(player){
+  return WIN_LINES.some(line => line.every(idx => board[idx] === player));
+}
+
 function handleMove(i){
   if(board[i]) return;
   board[i] = current;
   cellEls[i].textContent = current;
+  if(checkWin(current)){ alert(current + ' wins!'); return; }
   current = current === 'X' ? 'O' : 'X';
 }
 
